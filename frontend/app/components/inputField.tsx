d2e65f2c1657e8b38115a97a1177ea6fb2e9021e
preethamraj25dec <preethamredmi@gmail.com>
2026-05-23 18:58:56 +0530

    changed input text color

--- frontend/app/components/inputField.tsx
+++ frontend/app/components/inputField.tsx
@@ -16,7 +16,7 @@ export default function InputField()
       }
     return (
         <div className="flex border border-coffee w-1/2 items-center rounded-full justify-between p-2">
-            <input type='text' className="outline-0 rounded-full h-full w-full px-2" />
+            <input type='text' className="outline-0 rounded-full h-full w-full px-2 text-coffee " />
             <button onClick={callBackend}>
                 <SendIcon className={"w-auto h-6"} />
             </button>
